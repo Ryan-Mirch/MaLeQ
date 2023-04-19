2387abb1b91e342101861c64bfd8f6eb5d502204
Ryan Mirch <rwmirch@gmail.com>
2023-04-18 23:02:34 -0400

    baby making works

--- Manager.gd
+++ Manager.gd
@@ -29,14 +29,18 @@ func _on_start_generation_pressed() -> void:
 		
 
 func _on_end_generation_pressed() -> void:
+	#Freeze everything
 	for q in activeQuadrupeds:
 		q.freeze = true
-		
+	
+	#Sort based on fitness
 	activeQuadrupeds.sort_custom(_sort_by_fitness)
 	
+	#Print fitness levels
 	for q in activeQuadrupeds:
 		print(q.name + ": " + str(q.calculate_fitness()))
 	
+	#Keeping survivors
 	for q in activeQuadrupeds:		
 		q = q as Quadruped
 		q.fitnessLabel.text = str(snapped(q.calculate_fitness(),0.01))
@@ -45,15 +49,50 @@ func _on_end_generation_pressed() -> void:
 		if survivors.size() >= SURVIVORS_KEPT:
 			break
 	
+	#Destroying non-survivors
 	for q in activeQuadrupeds:
 		if !survivors.has(q):
 			q.queue_free()
 			
 	activeQuadrupeds.clear()
 	
+	create_baby(survivors[0], survivors[1])
+	
+	
 func _sort_by_fitness(q1, q2) -> bool:
 	return q1.calculate_fitness() > q2.calculate_fitness()
 	
+	
+func create_baby(q1: Quadruped, q2: Quadruped) -> Quadruped:
+	var arrayLength = q1.ARRAY_LENGTH
+	var splicePoint = randi_range(arrayLength*0.25, arrayLength*0.75)
+	var newData = {
+	"Leg1Femur"=[],
+	"Leg2Femur"=[], 
+	"Leg3Femur"=[], 
+	"Leg4Femur"=[], 
+	"Leg1Tibia"=[],
+	"Leg2Tibia"=[],
+	"Leg3Tibia"=[],
+	"Leg4Tibia"=[]
+	}
+	
+	for i in arrayLength:
+		if i < splicePoint:
+			for key in q1.data.keys():
+				newData[key].append(q1.data[key][i])
+		else:
+			for key in q2.data.keys():
+				newData[key].append(q2.data[key][i])
+				
+	print("Splice Point: " + str(splicePoint))
+	print(q1.data["Leg1Femur"])
+	print(q2.data["Leg1Femur"])
+	print(newData["Leg1Femur"])
+	
+	var baby = QUADRUPED_SCENE.instantiate()
+	baby.data = newData
+	return baby
 	
 
 	
